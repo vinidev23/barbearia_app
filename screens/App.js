be3f4149202a5f17f9e4e6d21f0a70096555bbdf
vinidev23 <vinidev23>
2025-10-27 21:17:42 -0300

    feat: Definindo as rotas da aplicação

---
+++ screens/App.js
@@ -0,0 +1,22 @@
+import { NavigationContainer, navigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigation } from '@react-navigation/native-stack';
+import WelcomeScreen from './WelcomeScreen';
+import NameScreen from './screens/NameScreen';
+import ServiceScreen from './screens/ServiceScreen';
+import ScheduleScreen from './screens/ScheduleScreen';
+import ConfirmationScreen from './screens/ConfirmationScreen';
+
+const Stack = createNativeStackNavigation();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Welcome" />
+                <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Name" component={NameScreen} />
+                <Stack.Screen name="Service" component={ServiceScreen} />
+                <Stack.Screen name="Schedule" component={ScheduleScreen} />
+                <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+        </NavigationContainer>
+    );
+}
